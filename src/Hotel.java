72aad74903cb8c60cd7de474a5fd5c014009136d
Anna Razinkina <anayrasi12341236@gmail.com>
2026-04-13 10:14:48 +0300

    Update hotel booking logic with dynamic date overlap and refactor DB schema

--- src/Hotel.java
+++ src/Hotel.java
@@ -3,46 +3,61 @@ public class Hotel implements Serializable {
     private int id;
     private String name;
     private String city;
-    private int rooms;
+    private int roomsTotal;
+    private int availableRooms;
     private double price;
-    public Hotel(int id, String name, String city, int rooms, double price){
-        this.id =id;
+
+    public Hotel(int id, String name, String city, int roomsTotal, double price) {
+        this.id = id;
         this.name = name;
         this.city = city;
-        this.rooms = rooms;
+        this.roomsTotal = roomsTotal;
         this.price = price;
     }
-    @Override
-    public String toString(){
-        return "Hotel [id=" + id + ", name=" + name + ", city=" + city + ", rooms=" + rooms + ", price=" + price + "]";
+
+    public int getId() {
+        return id;
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public int getRoomsTotal() {
+        return roomsTotal;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public int getAvailableRooms() {
+        return availableRooms;
+    }
+
+    public void setAvailableRooms(int availableRooms) {
+        this.availableRooms = availableRooms;
+    }
+
     @Override
-    public boolean equals(Object obj){
+    public String toString() {
+        return "Hotel [id=" + id + ", name=" + name + ", city=" + city + ", totalRooms=" + roomsTotal + "]";
+    }
+
+    @Override
+    public boolean equals(Object obj) {
         if (this == obj) return true;
         if (obj == null || getClass() != obj.getClass()) return false;
         Hotel hotel = (Hotel) obj;
         return id == hotel.id;
     }
+
     @Override
-    public int hashCode(){
+    public int hashCode() {
         return id;
-    }
-    public int getId() {
-        return id;
-    }
-    public String getName() {
-        return name;
-    }
-    public String getCity() {
-        return city;
-    }
-    public int getRooms() {
-        return rooms;
-    }
-    public double getPrice() {
-        return price;
-    }
-    public void setRooms(int rooms) {
-        this.rooms = rooms;
     }
 }
